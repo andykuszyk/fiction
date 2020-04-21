3cdc3cf17c7ad219a90a92486cab00b561e8f86b
Andy Kuszyk <pairofsocks@hotmail.co.uk>
2020-04-21 19:51:44 +0100

    Removed port mapping from deployed file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 commit=$(git log -n 1 --oneline | awk '{print $1}')
 sed -i "s/build:.*/image: andykuszyk\/akuszyk:$commit/g" docker-compose.yml
+sed -i 's/.*port.*//g' docker-compose.yml
+sed -i 's/.*80.*//g' docker-compose.yml
 scp -i id_rsa -o "StrictHostKeyChecking no" ./docker-compose.yml $douser@$doip:~/docker-compose.yml
 ssh -i id_rsa -o "StrictHostKeyChecking no" $douser@$doip "docker stack deploy -c ~/docker-compose.yml szyk"
